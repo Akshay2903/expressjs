a98c4f9bd9587557ccfa4fa8fbfd6ad7d5975874
Akshay Pal <akshaypal2903@gmail.com>
2023-09-11 13:51:26 +0530

    second commit

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use((req,res,next) => {
-    console.log("In the middleware")
-    next()
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use("/add-product",(req,res,next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"></br><input type="number" name="size"/></br><button type="submit">Add Product</button></form>')
+
 })
 
-app.use((req,res,next) => {
+app.post('/product', (req,res,next) => {
+    console.log(req.body.title, req.body.size)
+    res.redirect('/')
+})
+
+app.use("/",(req,res,next) => {
     res.send('<h1>Welcome to ExpressJs</h1>')
 })
 
